4c5e88367087cfc9fb74fce5de588059a6ee5ed2
ngothanhtan <oh.sick.cf@gmail.com>
2020-03-23 14:59:07 +0700

    login with fb

---
+++ New_Web_Blog/app/Http/Controllers/SocialAuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialAuthController extends Controller
+{
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function callback($provider)
+    {
+        $getInfo = Socialite::driver($provider)->stateless()->user();
+        $user = $this->findOfCreate($getInfo);
+        Auth::login($user);
+        return redirect()->to('/home');
+    }
+    public function findOfCreate($getInfo) {
+        $user = User::where('email', $getInfo->email)->first();
+        if (!$user)
+        {
+            $user = User::create([
+                'name' => $getInfo->name,
+                'password' => Hash::make($getInfo->id),
+                'email' => $getInfo->email,
+                'role' => 2,
+            ]);
+        }
+        return $user;
+    }
+}
--- New_Web_Blog/routes/web.php
+++ New_Web_Blog/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/redirect/{social}', 'SocialAuthController@redirect');
+Route::get('/callback/{social}', 'SocialAuthController@callback');
